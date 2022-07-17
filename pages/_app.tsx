60c1d834908f45c835cc8f712d3c216094432eb8
Simon Norman <zhymon.norman@gmail.com>
2022-07-17 17:58:39 -0400

    feat: add styling and prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
-import '../styles/globals.css'
-import '@fontsource/roboto-condensed';
-import '@fontsource/encode-sans';
-import '@fontsource/bebas-neue';
+import "../styles/globals.css";
+import "@fontsource/roboto-condensed";
+import "@fontsource/roboto-condensed/700.css";
+import "@fontsource/encode-sans";
+import "@fontsource/encode-sans/700.css";
+import "@fontsource/bebas-neue";
 
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,25 @@
 /* eslint-disable global-require */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: 'media',
+  darkMode: "media",
   theme: {
-    colors: {
-        bitterLemon: '#C9DB03',
-        brightNavyBlue: '#0077DE',
-        rufous: '#A81C07',
-        titaniumYellow: '#E6E200',
-        errieBlack: '#24250F',
-        white: '#f4f5f7',
+    extend: {
+      colors: {
+        bitterLemon: "#C9DB03",
+        brightNavyBlue: "#0077DE",
+        rufous: "#A81C07",
+        titaniumYellow: "#E6E200",
+        errieBlack: "#24250F",
+        white: "#f4f5f7",
       },
-    fontFamily: {
-      primary: ['Bebas Neue', 'sans-serif'],
-      secondary: ['Roboto Condensed', 'sans-serif'],
-      tertiary: ['Encode Sans', 'sans-serif'],
+      fontFamily: {
+        heading: ["Bebas Neue", "sans-serif"],
+        secondary: ["Roboto Condensed", "sans-serif"],
+        body: ["Encode Sans", "sans-serif"],
+      },
     },
   },
 };
